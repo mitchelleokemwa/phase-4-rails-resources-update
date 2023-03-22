258ed6d79fbe683af09f6e8a48226bb7421dc2e3
git_mitchelleokemwa <mitchelleokemwa@gmail.com>
2023-03-22 23:58:08 +0300

    Completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,31 +1,54 @@
-class BirdsController < ApplicationController
-
-  # GET /birds
-  def index
-    birds = Bird.all
-    render json: birds
-  end
-
-  # POST /birds
-  def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
-  end
-
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
-  end
-
-  private
-
-  def bird_params
-    params.permit(:name, :species)
-  end
-
-end
+class BirdsController < ApplicationController 
+  
+  # GET /birds  
+  def index  
+    birds = Bird.all  
+    render json: birds  
+  end  
+ 
+  # POST /birds  
+  def create  
+    bird = Bird.create(bird_params)  
+    render json: bird, status: :created  
+  end  
+  
+  # GET /birds/:id  
+  def show  
+    bird = Bird.find_by(id: params[:id])  
+    if bird 
+      render json: bird  
+    else 
+      render json: { error: "Bird not found" }, status: :not_found 
+    end 
+  end 
+ 
+ 
+  # PATCH /birds/:id 
+  def update 
+    bird = Bird.find_by(id: params[:id]) 
+    if bird 
+      bird.update(bird_params) 
+      render json: bird 
+    else 
+      render json: { error: "Bird not found" }, status: :not_found 
+    end 
+  end 
+ 
+  # PATCH likes to a bird 
+  def increment_likes 
+    bird = Bird.find_by(id: params[:id]) 
+    if bird 
+      bird.update(likes: bird.likes + 1) 
+      render json: bird 
+    else 
+      render json: { error: "Bird not found" }, status: :not_found 
+    end 
+  end 
+ 
+  private 
+ 
+  def bird_params 
+    params.permit(:name, :species, :likes ) 
+  end 
+ 
+end 
